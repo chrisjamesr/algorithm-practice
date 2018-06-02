7262cf167d79d413f9aade3d6b133c6802f2de73
chrisjamesr <chrisjames.r@gmail.com>
2018-06-02 09:57:17 -0400

    add comments

--- binary-search.rb
+++ binary-search.rb
@@ -3,17 +3,18 @@ def binary_search(list, item)
   high = list.length - 1
   i = 0
   while low <= high
-
+    # find midpoint of list
     mid = (low + high) /2
     guess = list[mid]
     puts "guess # #{i+=1} is #{guess}"
+    # check if guess equals item
     if guess == item
       # returns index of found item
       return mid 
     elsif guess > item 
-      high = mid -1 
+      high = mid -1 # sets new upper limit if guess is less than the item
     else
-      low = mid+1
+      low = mid+1 #sets new lower limit if guess is greater than the item
     end
   end  
   # return none
