11df98c2366a4cd771c23a5e4cab48d347601f76
chrisjamesr <chrisjames.r@gmail.com>
2018-06-06 19:13:00 -0400

    refactor sum and count using ternaty operator

--- recursion.rb
+++ recursion.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
+def return_array(x)
+  arr = []
+  x.times do
+    arr.push(Random.rand(10))
+  end
+  arr
+end
+# sum of items in a list
+
 def sum(arr)
   total = 0
   for x in arr;
@@ -8,15 +17,25 @@ def sum(arr)
   total
 end
 
-def sum_recurse(arr,total=0)
-  if arr.length >= 1 
-    total += arr.shift
-    recurse(arr, total)
-  else 
-    total
-  end
+# def sum_recurse(arr,total=0)
+#   if arr.length >= 1 
+#     total += arr.shift
+#     recurse(arr, total)
+#   else 
+#     total
+#   end
+# end
+
+def sum_recursion(arr,total=0)
+  arr.length >= 1 ? total += arr.shift : (return total)
+    sum_recursion(arr, total)
 end
 
-# load 'd-c-with-recursion.rb'
+# count number of items in a list
 
-a
+def count_recursion(list, count = 0)
+  list === [] ? (return count) : list.pop
+  count_recursion(list, count += 1)
+
+end
+
